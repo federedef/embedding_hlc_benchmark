3bb2c0109f66f3b367b786a088ee1a8ec37c0754
federedef <federogc98@gmail.com>
2025-03-23 20:58:40 +0100

    adding priorirization step

--- workflow.sh
+++ workflow.sh
@@ -1,4 +1,4 @@
-launch_RARE_[baseline;justnet;justcom;justcom_lou;netcom;netcom_lou]){
+%launch_RARE_[baseline;justnet;justcom;justcom_lou;netcom;netcom_lou]){
 	resources: -c 20 -m 100gb
 	source ~soft_bio_267/initializes/init_python
 	export PATH=$code_path:$PATH
@@ -41,7 +41,7 @@ launch_RARE_[baseline;justnet;justcom;justcom_lou;netcom;netcom_lou]){
 	mv kernel_matrix_bin.npy embedding_matrix.npy
 }
 
-get_spearman_corr){
+%get_spearman_corr){
 	resources: -c 20 -m 20gb 
 	source ~soft_bio_267/initializes/init_python
 	export PATH=$template:$PATH
@@ -53,7 +53,7 @@ get_spearman_corr){
 	report_corr.py -R launch_RARE_justcom)/embedding_matrix.npy -G launch_RARE_justcom)/embedding_matrix.npy -r launch_RARE_justcom)/embedding_matrix.lst -g launch_RARE_justcom)/embedding_matrix.lst -t $template/corr.txt -o "justcom_justcom_$dataset"
 }
 
-get_quality_from_external_[baseline;justnet;justcom;justcom_lou;netcom;netcom_lou]){
+%get_quality_from_external_[baseline;justnet;justcom;justcom_lou;netcom;netcom_lou]){
 	resources: -c 20 -m 50gb -A exclusive
 	source ~soft_bio_267/initializes/init_python
 	PATH=$scripts_code:$PATH
@@ -70,12 +70,30 @@ get_quality_from_external_[baseline;justnet;justcom;justcom_lou;netcom;netcom_lo
 	fi
 }
 
+%ranking_groups_[baseline;justnet;justcom;justcom_lou;netcom;netcom_lou]){
+	resources: -m 50gb 
+	source ~soft_bio_267/initializes/init_python
+	source ~/dev_py/netenv/bin/activate
+	text2binary_matrix -i $input_path/edges/$dataset -t pair -O bin -o adj_matrix
+	text2binary_matrix -i !launch_RARE_*!/embedding_matrix.npy -t "bin" -O "bin" --coords2kernel "dotProduct" -o kernel --cosine_normalization
+	exectype=(*)
+	aggregate_column_data -i $external_path -x 1 -a 2 > seeds
+	?
+	ranker -k kernel.npy -n !launch_RARE_*!/embedding_matrix.lst --adj_matrix adj_matrix.npy --seed_nodes seeds \
+		--score2pvalue "logistic" --representation_seed_metric "bayesian" -t 20 --seed_presence "remove"
+	# ranker -k kernel.npy -n !launch_RARE_*!/embedding_matrix.lst --adj_matrix adj_matrix.npy --seed_nodes seeds --representation_seed_metric "mean" -t 5 --seed_presence "remove"
+	if [ -s ranked_genes_all_candidates ] ; then
+		awk -v exectype="$exectype" 'BEGIN{FS="\t";OFS="\t"}{print exectype,$0}' ranked_genes_all_candidates > final_ranked_genes_all_candidates
+	fi
+}
+
 report_quality){
 	source ~soft_bio_267/initializes/init_python
 	PATH=$template:$PATH
 	cat !get_quality_from_external_!/final_quality_metrics > quality_metrics
 	cat !get_quality_from_external_!/final_relative_quality_metrics > relative_quality_metrics
 	cat !get_quality_from_external_!/final_group_distance > final_group_distance
+	cat !ranking_groups_!/final_ranked_genes_all_candidates > final_ranked_genes_all_candidates
 	echo -e "baseline;launch_RARE_baseline)/embedding_matrix,\
 	justnet;launch_RARE_justnet)/embedding_matrix,\
 	justcom;launch_RARE_justcom)/embedding_matrix,\
@@ -97,7 +115,10 @@ report_quality){
 		--external_groups $external_path -o "quality_metrics_$dataset" \
 		--external_group_description "$external2description_path" \
 		--group_distance final_group_distance \
-		--embedding_distances `cat dist_data`
+		--embedding_distances `cat dist_data` \
+		--gene_names_ens $name_nodes \
+		--gene_names_symbol $name_nodes_symbol \
+		--prioritization final_ranked_genes_all_candidates
 	fi
 }
 
